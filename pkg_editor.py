574a43db243bf9cac2a9ec4bbd502f9a66af9c5b
johnnymo87 <johnnymo87@gmail.com>
2013-09-26 23:37:11 -0400

    upgrading script to allow mass assignment of attributes

--- pkg_editor.py
+++ pkg_editor.py
@@ -1,19 +1,72 @@
 from ApexScrape import *
+import string
 
-FLGAN = Apex(('FLGANStore', 'password'))
 
-params = OrderedDict((
-            ('actionSequence', '3'),
+class Editor(Apex):
+
+    def __init__(self, (username, password), customer, filename):
+        Apex.__init__(self, (username, password))
+        self.target = self.matchCustomer(customer)
+        print self.target
+        self.records = self.readTemplate(filename)
+
+    def matchCustomer(self, customer):
+        for cust in self.customers:
+            if string.lower(customer) in string.lower(cust):
+                print 'Customer set to', cust
+                return {'comId': self.customers[cust], 'siteId': self.comId}
+        else:
+            raise LookupError(self.target, 'not found!')
+
+    def readTemplate(self, filename):
+        """line[0] = SKU, line[1] = Description"""
+        records = {}
+        with open(filename, 'r') as f:
+            for line in f:
+                line = line.strip().split('\t')
+                records[line[0]] = line[1]
+##        print records
+        return records
+
+    def updateName(self, SKU):
+        print 'Updating name for', SKU, ':', self.records[SKU]
+        url = "https://fastsolutions.mroadmin.com/ProductManager/product_listSiteProductAjax.action"
+        params = OrderedDict((
+            ('actionSequence', 3),
             ('RequestId', 'undefined'),
-            ('newSortColumn', '0'),
-            ('oldSortColumn', '0'),
+            ('newSortColumn', 0),
+            ('oldSortColumn', 0),
             ('sortFlag', 'null'),
-            ('sortCancel', '0'),
-            ('companyId', 'COM000002212'),
-            ('siteId', 'SIT100110786'),
-            ('searchText', '0200803')
-        ))
-request = FLGAN.s.post("https://fastsolutions.mroadmin.com/ProductManager/product_listSiteProductAjax.action", params=params)      
+            ('sortCancel', 0),
+            ('companyId', self.target['comId']),
+            ('siteId', self.target['siteId']),
+            ('searchText', SKU)
+            ))
+        request = self.s.post(url, params)
+        data = request.content.split('|')
+        data = [json.loads(d) for d in data]
+        return data[7:][0]
+
+if __name__ == '__main__':
+    editor = Editor(('STOREFLORM', 'password'), 'Hudson Tool Tool Room', 'Hudson Rename.txt')
+    first = editor.records.keys()[0]
+    data = editor.updateName(first)
+    
+
+##FLGAN = Apex(('FLGANStore', 'password'))
+##
+##params = OrderedDict((
+##            ('actionSequence', '3'),
+##            ('RequestId', 'undefined'),
+##            ('newSortColumn', '0'),
+##            ('oldSortColumn', '0'),
+##            ('sortFlag', 'null'),
+##            ('sortCancel', '0'),
+##            ('companyId', 'COM000002212'),
+##            ('siteId', 'SIT100110786'),
+##            ('searchText', '0200803')
+##        ))
+##request = FLGAN.s.post("https://fastsolutions.mroadmin.com/ProductManager/product_listSiteProductAjax.action", params=params)      
 
 ##url = 'https://fastsolutions.mroadmin.com/ProductManager/product_editSiteProduct.action'
 ##params = OrderedDict((
@@ -39,4 +92,4 @@ request = FLGAN.s.post("https://fastsolutions.mroadmin.com/ProductManager/produc
 ##	))
 ##
 ##request = FLGAN.s.post(url, params)
-print request.content
+##print request.content
