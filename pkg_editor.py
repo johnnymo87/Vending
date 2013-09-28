024ee216d7b0ad2e7392245ec0d3bcf9c58f8575
johnnymo87 <johnnymo87@gmail.com>
2013-09-27 23:13:32 -0400

    now can edit any standard attributes

--- pkg_editor.py
+++ pkg_editor.py
@@ -4,32 +4,20 @@ import string
 
 class Editor(Apex):
 
-    def __init__(self, (username, password), customer, filename):
+    def __init__(self, (username, password), customer):
         Apex.__init__(self, (username, password))
         self.target = self.matchCustomer(customer)
         print self.target
-        self.records = self.readTemplate(filename)
 
     def matchCustomer(self, customer):
         for cust in self.customers:
             if string.lower(customer) in string.lower(cust):
                 print 'Customer set to', cust
-                return {'comId': self.customers[cust], 'siteId': self.comId}
+                return {'comId': self.comId, 'siteId': self.customers[cust]}
         else:
             raise LookupError(self.target, 'not found!')
 
-    def readTemplate(self, filename):
-        """line[0] = SKU, line[1] = Description"""
-        records = {}
-        with open(filename, 'r') as f:
-            for line in f:
-                line = line.strip().split('\t')
-                records[line[0]] = line[1]
-##        print records
-        return records
-
-    def updateName(self, SKU):
-        print 'Updating name for', SKU, ':', self.records[SKU]
+    def findParts(self, searchText):
         url = "https://fastsolutions.mroadmin.com/ProductManager/product_listSiteProductAjax.action"
         params = OrderedDict((
             ('actionSequence', 3),
@@ -40,56 +28,82 @@ class Editor(Apex):
             ('sortCancel', 0),
             ('companyId', self.target['comId']),
             ('siteId', self.target['siteId']),
-            ('searchText', SKU)
+            ('searchText', searchText)
             ))
         request = self.s.post(url, params)
         data = request.content.split('|')
         data = [json.loads(d) for d in data]
-        return data[7:][0]
+        if len(data[6]) < 1:
+            raise LookupError('No parts found for', SKU)
+        return data[6]
+
+    def updatePart(self, part, updates):
+        for up in updates:
+            assert up in part
+            part[up] = updates[up]
+
+    def postChanges(self, part):
+        print 'Updating {} : {}'.format(part['productNum1'], part['name'])
+        url = 'https://fastsolutions.mroadmin.com/ProductManager/product_editSiteProduct.action'                
+        params = OrderedDict((
+                    ('siteProductId', 'editSiteProduct.site_product_id'),
+                    ('name', 'editSiteProduct.name'),
+                    ('productNum1', 'editSiteProduct.product_num1'),
+                    ('productNum2', 'editSiteProduct.product_num2'),
+                    ('productNum3', 'editSiteProduct.product_num3'),
+                    ('productNam1', 'editSiteProduct.product_name1'),
+                    ('ManufacturerName', 'editSiteProduct.manufacturer_name'),
+                    ('productSize', 'editSiteProduct.product_size'),
+                    ('productColor', 'editSiteProduct.product_color'),
+                    ('unitPrice', 'editSiteProduct.unit_price'),
+                    ('packagePrice', 'editSiteProduct.package_price'),
+                    ('packageQty', 'editSiteProduct.package_qty'),
+                    ('unitCost', 'editSiteProduct.unit_cost'),
+                    ('description', 'editSiteProduct.description'),
+                    ('supplierId', 'editSiteProduct.supplier_id'),
+                    ('siteId', 'editSiteProduct.site_id'),
+                    ('isCritical', 'editSiteProduct.is_critical'),
+                    ('productGroupId', 'editSiteProduct.product_group_id'),
+                    ('flagEdit', 'flagEdit')
+                ))
+        real_params = OrderedDict()
+        for param in params:
+            if param in part:
+                real_params[params[param]] = str(part[param])
+            else:
+                real_params[params[param]] = ''
+        request = self.s.post(url, real_params)
+
+           
+
+
+def readTemplate(filename):
+    """line[0] = SKU, line[1] = Description"""
+    attributes = ['ManufacturerName', 'description', 'flagEdit', 'isCritical', 'name', \
+                  'packagePrice', 'packageQty', 'productColor', 'productGroupId', \
+                  'productNam1', 'productNum1', 'productNum2', 'productNum3', \
+                  'productSize', 'siteId', 'siteProductId', 'supplierId', 'unitCost', \
+                  'unitPrice']
+    updates = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip().split('\t')
+            if len(line) % 2 != 1:
+                raise ValueError('Even number of columns, cannot make key-value pairs')
+            pairs = line[1:]
+            for p in pairs[0::2]:
+                assert p in attributes
+            updates[line[0]] = dict(zip(pairs[0::2], pairs[1::2]))
+    return updates
+        
 
 if __name__ == '__main__':
-    editor = Editor(('STOREFLORM', 'password'), 'Hudson Tool Tool Room', 'Hudson Rename.txt')
-    first = editor.records.keys()[0]
-    data = editor.updateName(first)
-    
-
-##FLGAN = Apex(('FLGANStore', 'password'))
-##
-##params = OrderedDict((
-##            ('actionSequence', '3'),
-##            ('RequestId', 'undefined'),
-##            ('newSortColumn', '0'),
-##            ('oldSortColumn', '0'),
-##            ('sortFlag', 'null'),
-##            ('sortCancel', '0'),
-##            ('companyId', 'COM000002212'),
-##            ('siteId', 'SIT100110786'),
-##            ('searchText', '0200803')
-##        ))
-##request = FLGAN.s.post("https://fastsolutions.mroadmin.com/ProductManager/product_listSiteProductAjax.action", params=params)      
-
-##url = 'https://fastsolutions.mroadmin.com/ProductManager/product_editSiteProduct.action'
-##params = OrderedDict((
-##            ('editSiteProduct.site_product_id', 'SPD100688935'),
-##            ('editSiteProduct.name', 'Yellow Stick Marker 3pk'),
-##            ('editSiteProduct.product_num1', '0200803'),
-##            ('editSiteProduct.product_num2', ''),
-##            ('editSiteProduct.product_num3', ''),
-##            ('editSiteProduct.product_name1', ''),
-##            ('editSiteProduct.manufacturer_name', ''),
-##            ('editSiteProduct.product_size', ''),
-##            ('editSiteProduct.product_color', ''),
-##            ('editSiteProduct.unit_price', 5.13),
-##            ('editSiteProduct.package_price', 15.39),
-##            ('editSiteProduct.package_qty', 1),
-##            ('editSiteProduct.unit_cost', 0.01),
-##            ('editSiteProduct.description', 'Yellow Stick Marker 3pk'),
-##            ('editSiteProduct.supplier_id', 'COM000002212'),
-##            ('editSiteProduct.site_id', 'SIT100110786'),
-##            ('editSiteProduct.is_critical', 'Y'),
-##            ('editSiteProduct.product_group_id', ''),
-##            ('flagEdit', '')
-##	))
-##
-##request = FLGAN.s.post(url, params)
-##print request.content
+    updates = readTemplate('Hudson Rename.txt')
+    editor = Editor(('STOREFLORM', 'password'), 'Hudson Tool Tool Room')
+    for up in updates:
+        parts = editor.findParts(up)
+        part = [ p for p in parts if p['productNum1'] == up ]
+        assert len(part) == 1
+        part = part[0]
+        editor.updatePart(part, updates[up])
+        editor.postChanges(part)
